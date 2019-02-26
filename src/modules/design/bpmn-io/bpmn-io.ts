9387a2cc520d3cfd3d0fda7c6ad66de6dc99cc8e
Vyperus <bastian.schnittger@5minds.de>
2019-02-26 14:44:54 +0100

    :fire: Delete Obsolete Ones

--- src/modules/design/bpmn-io/bpmn-io.ts
+++ src/modules/design/bpmn-io/bpmn-io.ts
@@ -130,23 +130,6 @@ export class BpmnIo {
     }, handlerPriority);
 
     this.modeler.on(['shape.added', 'shape.removed'], (event: IInternalEvent) => {
-      const shapeIsParticipant: boolean = event.element.type === 'bpmn:Participant';
-
-      if (shapeIsParticipant) {
-        return this._checkForMultipleParticipants(event);
-      }
-    });
-
-    this.modeler.on('shape.remove', (event: IInternalEvent) => {
-      const shapeIsParticipant: boolean = event.element.type === 'bpmn:Participant';
-      if (shapeIsParticipant) {
-        const rootElements: Array<IProcessRef> = this.modeler._definitions.rootElements;
-        this._tempProcess = rootElements.find((element: IProcessRef) => {
-          return element.$type === 'bpmn:Process';
-        });
-
-        return event;
-      }
       this._validateDiagram();
     });
 
@@ -179,8 +162,6 @@ export class BpmnIo {
         this._bpmnLintButton = document.querySelector('.bpmn-js-bpmnlint-button');
 
         this._bpmnLintButton.style.display = 'none';
-
-        this._linting = this.modeler.get('linting');
       }, 0);
     }
 
@@ -506,34 +487,6 @@ export class BpmnIo {
     }
 
     return randomId;
-  }
-
-  private _checkForMultipleParticipants(event: IInternalEvent): IInternalEvent {
-    const elementRegistry: IElementRegistry = this.modeler.get('elementRegistry');
-
-    setTimeout(() => {
-      const participants: Array<IShape> = elementRegistry.filter((element: IShape) => {
-        return element.type === 'bpmn:Participant';
-      });
-
-      const multipleParticipants: boolean = participants.length > 1;
-
-      if (this._diagramHasChanges) {
-        participants.forEach((participant: IShape) => {
-          participant.businessObject.processRef.id = this._tempProcess.id;
-          participant.businessObject.processRef.isExecutable = this._tempProcess.isExecutable;
-        });
-      }
-
-      const eventToPublish: string = multipleParticipants
-                                     ? environment.events.navBar.validationError
-                                     : environment.events.navBar.noValidationError;
-
-      this._eventAggregator.publish(eventToPublish);
-
-    }, elementRegistryTimeoutMilliseconds);
-
-    return event;
   }
 
   private _setNewPropertyPanelWidthFromMousePosition(mousePosition: number): void {
