0c212fb5d5f2ea78063472c45495bb654592f5bd
MaFa961 <marcel.fasterding@5minds.de>
2019-03-04 10:12:50 +0100

    :recycle: Change Copyright to 2019

--- electron_app/electron.js
+++ electron_app/electron.js
@@ -372,7 +372,7 @@ Main._createMainWindow = function () {
               product_name: 'BPMN-Studio',
               bug_report_url: 'https://github.com/process-engine/bpmn-studio/issues/new',
               homepage: 'www.process-engine.io',
-              copyright: 'Copyright © 2018 process-engine',
+              copyright: 'Copyright © 2019 process-engine',
               win_options: {
                 minimizable: false,
                 maximizable: false,
