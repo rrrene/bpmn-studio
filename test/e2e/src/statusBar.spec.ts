85b3eccbaf63da2c74fd830ad3ac7bce5e2db072
Vyperus <bastian.schnittger@5minds.de>
2019-03-11 16:42:41 +0100

    :recycle: Refactor Status Bar Tests

--- test/e2e/src/statusBar.spec.ts
+++ test/e2e/src/statusBar.spec.ts
@@ -27,15 +27,13 @@ describe('Status Bar', () => {
     diffView = new DiffView(applicationUrl, diagram.name);
 
     await diagram.deployDiagram();
+
+    await routerView.show();
+    await statusBar.show();
   });
 
   afterAll(async() => {
     await diagram.deleteDiagram();
-  });
-
-  beforeEach(async() => {
-    await routerView.show();
-    await statusBar.show();
   });
 
   it('should contain left container.', async() => {
@@ -57,7 +55,9 @@ describe('Status Bar', () => {
   });
 
   it('(on diagram detail) should show enable xml view button.', async() => {
+    await routerView.show();
     await diagramDetail.show();
+    await statusBar.show();
 
     const visibilityOfEnableXmlViewButton: boolean = await statusBar.getVisibilityOfEnableXmlViewButton();
 
@@ -65,16 +65,12 @@ describe('Status Bar', () => {
   });
 
   it('should show  the `enable diff view` button on the `diagram detail view`.', async() => {
-    await diagramDetail.show();
-
     const visibilityOfEnableDiffViewButton: boolean = await statusBar.getVisibilityOfEnableDiffViewButton();
 
     expect(visibilityOfEnableDiffViewButton).toBeTruthy();
   });
 
   it('should show the `disable xml view button` on the `diagram detail view`, after clicking on the `enable xml view` button.', async() => {
-    await diagramDetail.show();
-
     await statusBar.clickOnEnableXmlViewButton();
 
     const visibilityOfDisableXmlViewButton: boolean = await statusBar.getVisibilityOfDisableXmlViewButton();
@@ -82,35 +78,19 @@ describe('Status Bar', () => {
     expect(visibilityOfDisableXmlViewButton).toBeTruthy();
   });
 
-  it('should show the `disable diff view` button on the `diagram detail view`, after clicking on the `enable diff view` button.', async() => {
-    await diagramDetail.show();
-
-    await statusBar.clickOnEnableDiffViewButton();
-
-    const visibilityOfDisableDiffViewButton: boolean = await statusBar.getVisibilityOfDisableDiffViewButton();
-
-    expect(visibilityOfDisableDiffViewButton).toBeTruthy();
-  });
-
   it('should show the `disable xml view` button on the `xml view`.', async() => {
-    await xmlView.show();
-
     const visibilityOfDisableXmlViewButton: boolean = await statusBar.getVisibilityOfDisableXmlViewButton();
 
     expect(visibilityOfDisableXmlViewButton).toBeTruthy();
   });
 
   it('should show the `enable diff view` button on the `xml view`.', async() => {
-    await xmlView.show();
-
     const visibilityOfEnableDiffViewButton: boolean = await statusBar.getVisibilityOfEnableDiffViewButton();
 
     expect(visibilityOfEnableDiffViewButton).toBeTruthy();
   });
 
   it('should show the `enable xml view` button on the `xml view`, after clicking on the `disable xml view` button', async() => {
-    await xmlView.show();
-
     await statusBar.clickOnDisableXmlViewButton();
 
     const visibilityOfEnableXmlViewButton: boolean = await statusBar.getVisibilityOfEnableXmlViewButton();
@@ -118,33 +98,33 @@ describe('Status Bar', () => {
     expect(visibilityOfEnableXmlViewButton).toBeTruthy();
   });
 
-  it('should `show disable diff view` button on `diff view`.', async() => {
-    await diffView.show();
+  it('should show the `disable diff view` button on the `diagram detail view`, after clicking on the `enable diff view` button.', async() => {
+    await statusBar.clickOnEnableDiffViewButton();
 
     const visibilityOfDisableDiffViewButton: boolean = await statusBar.getVisibilityOfDisableDiffViewButton();
 
     expect(visibilityOfDisableDiffViewButton).toBeTruthy();
   });
 
-  it('should show `enable xml view` button on the `diff view`.', async() => {
-    await diffView.show();
+  it('should `show disable diff view` button on `diff view`.', async() => {
+    const visibilityOfDisableDiffViewButton: boolean = await statusBar.getVisibilityOfDisableDiffViewButton();
 
+    expect(visibilityOfDisableDiffViewButton).toBeTruthy();
+  });
+
+  it('should show `enable xml view` button on the `diff view`.', async() => {
     const visibilityOfEnableXmlViewButton: boolean = await statusBar.getVisibilityOfEnableXmlViewButton();
 
     expect(visibilityOfEnableXmlViewButton).toBeTruthy();
   });
 
   it('should contain the `old vs new` button on the `diff view.`', async() => {
-    await diffView.show();
-
     const visibilityOfOldVsNewButton: boolean = await statusBar.getVisibilityOfOldVsNewButton();
 
     expect(visibilityOfOldVsNewButton).toBeTruthy();
   });
 
   it('should contain the `active new vs old` button on the `diff view`.', async() => {
-    await diffView.show();
-
     const visbilityOfNewVsOldButton: boolean = await statusBar.getVisibilityOfNewVsOldButton();
 
     expect(visbilityOfNewVsOldButton).toBeTruthy();
@@ -155,16 +135,12 @@ describe('Status Bar', () => {
   });
 
   it('should contain the changelog button on the `diff view`.', async() => {
-    await diffView.show();
-
     const visibilityOfChangeLogButton: boolean = await statusBar.getVisibilityOfChangeLogButton();
 
     expect(visibilityOfChangeLogButton).toBeTruthy();
   });
 
   it('should contain the `active old vs new button` on the `diff view`, after clicking on it.', async() => {
-    await diffView.show();
-
     await statusBar.clickOnOldVsNewButton();
 
     const activeStateOfOldVsNewButton: boolean = await statusBar.getActiveStateOfOldVsNewButton();
@@ -173,8 +149,6 @@ describe('Status Bar', () => {
   });
 
   it('should contain the `inactive new vs old button` on the`diff view`, after clicking on the `old vs new` button.', async() => {
-    await diffView.show();
-
     await statusBar.clickOnOldVsNewButton();
 
     const activeStateOfNewVsOldButton: boolean = await statusBar.getActiveStateOfNewVsOldButton();
@@ -183,8 +157,6 @@ describe('Status Bar', () => {
   });
 
   it('should contain the `active change log` button on the `diff view`, after clicking on it.', async() => {
-    await diffView.show();
-
     await statusBar.clickOnChangeLogButton();
 
     const activeStateOfChangeLogButton: boolean = await statusBar.getActiveStateOfChangeLogButton();
