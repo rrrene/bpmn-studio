9d69b12aaf4819cbf218d3963f872da92e1f7c89
Alex😎 <alexander.kasten@5minds.de>
2019-05-27 11:25:13 +0200

    :sparkles: Add Own TokenViewerService and Repository

---
+++ src/modules/inspect/token-viewer/repository/token-viewer.repository.ts
@@ -0,0 +1,35 @@
+import {inject} from 'aurelia-framework';
+
+import {IIdentity} from '@essential-projects/iam_contracts';
+import {DataModels, IManagementApi} from '@process-engine/management_api_contracts';
+
+import {ITokenViewerRepository} from '../contracts';
+
+@inject('ManagementApiClientService')
+export class TokenViewerRepository implements ITokenViewerRepository {
+
+  private managementApiService: IManagementApi;
+
+  constructor(managementApiService: IManagementApi) {
+    this.managementApiService = managementApiService;
+  }
+
+  public async getTokenForFlowNodeInstance(
+    processModelId: string,
+    correlationId: string,
+    flowNodeId: string,
+    identity: IIdentity,
+  ): Promise<Array<DataModels.TokenHistory.TokenHistoryEntry>> {
+
+    return this.managementApiService.getTokensForFlowNode(identity, correlationId, processModelId, flowNodeId);
+  }
+
+  public async getTokenForFlowNodeByProcessInstanceId(
+    processInstanceId: string,
+    flowNodeId: string,
+    identity: IIdentity,
+  ): Promise<DataModels.TokenHistory.TokenHistoryGroup> {
+
+    return this.managementApiService.getTokensForFlowNodeByProcessInstanceId(identity, processInstanceId, flowNodeId);
+  }
+}
