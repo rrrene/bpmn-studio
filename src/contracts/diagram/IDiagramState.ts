411297bac4ac5008581d85835c6ac0b91749cac7
Steffen <Steffen.knaup@5minds.de>
2019-06-17 16:20:14 +0200

    :lipstick: Add Type for Location

--- src/contracts/diagram/IDiagramState.ts
+++ src/contracts/diagram/IDiagramState.ts
@@ -1,11 +1,12 @@
 import {IShape} from '@process-engine/bpmn-elements_contracts';
+import {IViewbox} from '../index';
 
 export interface IDiagramState {
   data: {
     xml: string,
   };
   metaData: {
-    location: any,
+    location: IViewbox,
     selectedElements: Array<IShape>,
   };
 }
