f7c066654ff1a433598d1496be11dfe655c278d0
Steffen <Steffen.knaup@5minds.de>
2019-06-17 12:21:16 +0200

    :sparkles: Add Interface for DiagramState

---
+++ src/contracts/diagram/IDiagramState.ts
@@ -0,0 +1,11 @@
+import { IShape } from '@process-engine/bpmn-elements_contracts';
+
+export interface IDiagramState {
+  data: {
+    xml: string,
+  };
+  metaData: {
+    location: any,
+    selectedElements: Array<IShape>,
+  };
+}
