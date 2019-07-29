9e88332873259f7c0fca049b7d93bf25744a9199
Alex😎 <alexander.kasten@5minds.de>
2019-07-29 12:11:02 +0200

    :rotating_light: Fix errors on diagram-svg-service

--- src/modules/design/bpmn-io/services/diagram.svg.converter.ts
+++ src/modules/design/bpmn-io/services/diagram.svg.converter.ts
@@ -2,54 +2,54 @@ import {IExportService, ISvgConvertService} from '../../../../contracts/index';
 import {ExportService} from './export.service';
 
 export class DiagramSvgConverter implements ISvgConvertService {
-  private _enqueuedPromises: Array<Promise<string>> = [];
-  private _svgContent: string;
+  private enqueuedPromises: Array<Promise<string>> = [];
+  private svgContent: string;
 
   constructor(svgContent: string) {
-    this._svgContent = svgContent;
+    this.svgContent = svgContent;
   }
 
   public asPNG(): IExportService {
-    const pngExporterPromise: Promise<string> = this._pngExporter();
+    const pngExporterPromise: Promise<string> = this.pngExporter();
     const mimeType: string = 'image/png';
 
-    this._enqueuedPromises.push(pngExporterPromise);
+    this.enqueuedPromises.push(pngExporterPromise);
 
-    return new ExportService(mimeType, this._enqueuedPromises);
+    return new ExportService(mimeType, this.enqueuedPromises);
   }
 
   public asJPEG(): IExportService {
-    const jpegExporterPromise: Promise<string> = this._jpegExporter();
+    const jpegExporterPromise: Promise<string> = this.jpegExporter();
     const mimeType: string = 'image/jpeg';
 
-    this._enqueuedPromises.push(jpegExporterPromise);
+    this.enqueuedPromises.push(jpegExporterPromise);
 
-    return new ExportService(mimeType, this._enqueuedPromises);
+    return new ExportService(mimeType, this.enqueuedPromises);
   }
 
   public asSVG(): IExportService {
     const mimeType: string = 'image/svg+xml';
     const svgExporterPromise: Promise<string> = new Promise((resolve: Function): void => {
-      resolve(this._svgContent);
+      resolve(this.svgContent);
     });
 
-    this._enqueuedPromises.push(svgExporterPromise);
+    this.enqueuedPromises.push(svgExporterPromise);
 
-    return new ExportService(mimeType, this._enqueuedPromises);
+    return new ExportService(mimeType, this.enqueuedPromises);
   }
 
   /**
    * Exports the current diagram as a PNG image.
    */
-  private _pngExporter = async (): Promise<string> => {
-    return this._generateImageFromSVG('png', this._svgContent);
+  private pngExporter = async (): Promise<string> => {
+    return this.generateImageFromSVG('png', this.svgContent);
   };
 
   /**
    * Exports the current diagram as a jpeg image.
    */
-  private _jpegExporter = async (): Promise<string> => {
-    return this._generateImageFromSVG('jpeg', this._svgContent);
+  private jpegExporter = async (): Promise<string> => {
+    return this.generateImageFromSVG('jpeg', this.svgContent);
   };
 
   /**
@@ -60,12 +60,14 @@ export class DiagramSvgConverter implements ISvgConvertService {
    * @param svg SVG that should be converted into an image with the desired format.
    * @returns A DataURL that points to the created image.
    */
-  private async _generateImageFromSVG(desiredImageType: string, svg: string): Promise<string> {
+  private async generateImageFromSVG(desiredImageType: string, svg: string): Promise<string> {
     const encoding: string = `image/${desiredImageType}`;
     const canvas: HTMLCanvasElement = document.createElement('canvas');
     const context: CanvasRenderingContext2D = canvas.getContext('2d');
 
+    // eslint-disable-next-line no-useless-escape
     const svgWidth: number = parseInt(svg.match(/<svg[^>]*width\s*=\s*\"?(\d+)\"?[^>]*>/)[1]);
+    // eslint-disable-next-line no-useless-escape
     const svgHeight: number = parseInt(svg.match(/<svg[^>]*height\s*=\s*\"?(\d+)\"?[^>]*>/)[1]);
 
     // For a print, we use 300 dpi
@@ -77,7 +79,7 @@ export class DiagramSvgConverter implements ISvgConvertService {
      * DIN A4 Paper, which has a diagonal size of 14.17 inches.
      */
     const dinA4DiagonalSizeInch: number = 14.17;
-    const pixelRatio: number = this._calculatePixelRatioForDPI(svgWidth, svgHeight, targetDPI, dinA4DiagonalSizeInch);
+    const pixelRatio: number = this.calculatePixelRatioForDPI(svgWidth, svgHeight, targetDPI, dinA4DiagonalSizeInch);
 
     canvas.width = svgWidth * pixelRatio;
     canvas.height = svgHeight * pixelRatio;
@@ -87,7 +89,7 @@ export class DiagramSvgConverter implements ISvgConvertService {
     context.fillRect(0, 0, canvas.width, canvas.height);
 
     // Draw the image to the canvas
-    const imageDataURL: string = await this._drawSVGToCanvas(svg, canvas, context, encoding);
+    const imageDataURL: string = await this.drawSVGToCanvas(svg, canvas, context, encoding);
 
     return imageDataURL;
   }
@@ -105,7 +107,7 @@ export class DiagramSvgConverter implements ISvgConvertService {
    * @returns The needed pixel ratio for the current dimensions to achieve the
    * desired DPI.
    */
-  private _calculatePixelRatioForDPI(
+  private calculatePixelRatioForDPI(
     svgWidth: number,
     svgHeight: number,
     targetDPI: number,
@@ -133,7 +135,7 @@ export class DiagramSvgConverter implements ISvgConvertService {
    * @param encoding Encoding of the output image.
    * @returns The URL which points to the rendered image.
    */
-  private async _drawSVGToCanvas(
+  private async drawSVGToCanvas(
     svgContent: string,
     canvas: HTMLCanvasElement,
     context: CanvasRenderingContext2D,
