38a6952c4984cc28ccf4a161be309bc003ee3c70
Alex😎 <alexander.kasten@5minds.de>
2019-07-29 12:14:49 +0200

    :rotating_light: Fix errors on general flow

--- src/modules/design/property-panel/indextabs/general/sections/flow/flow.ts
+++ src/modules/design/property-panel/indextabs/general/sections/flow/flow.ts
@@ -12,25 +12,25 @@ export class FlowSection implements ISection {
   public canHandleElement: boolean = false;
   public condition: string;
 
-  private _businessObjInPanel: IFlowElement;
-  private _moddle: IBpmnModdle;
-  private _eventAggregator: EventAggregator;
+  private businessObjInPanel: IFlowElement;
+  private moddle: IBpmnModdle;
+  private eventAggregator: EventAggregator;
 
   constructor(eventAggregator?: EventAggregator) {
-    this._eventAggregator = eventAggregator;
+    this.eventAggregator = eventAggregator;
   }
 
   public activate(model: IPageModel): void {
-    this._businessObjInPanel = model.elementInPanel.businessObject;
-    this._moddle = model.modeler.get('moddle');
+    this.businessObjInPanel = model.elementInPanel.businessObject;
+    this.moddle = model.modeler.get('moddle');
 
-    this._init();
+    this.init();
   }
 
   public isSuitableForElement(elementShape: IShape): boolean {
     if (elementShape !== undefined && elementShape !== null) {
       const element: IFlowElement = elementShape.businessObject;
-      if (!this._elementIsFlow(element)) {
+      if (!this.elementIsFlow(element)) {
         return false;
       }
 
@@ -48,42 +48,44 @@ export class FlowSection implements ISection {
 
       return flowHasCondition;
     }
+
+    return false;
   }
 
   public updateCondition(): void {
     const objectHasNoConditionExpression: boolean =
-      this._businessObjInPanel.conditionExpression === undefined ||
-      this._businessObjInPanel.conditionExpression === null;
+      this.businessObjInPanel.conditionExpression === undefined ||
+      this.businessObjInPanel.conditionExpression === null;
 
     if (objectHasNoConditionExpression) {
-      this._createConditionExpression();
+      this.createConditionExpression();
     }
 
-    this._businessObjInPanel.conditionExpression.body = this.condition;
-    this._publishDiagramChange();
+    this.businessObjInPanel.conditionExpression.body = this.condition;
+    this.publishDiagramChange();
   }
 
-  private _createConditionExpression(): void {
-    const conditionExpression: IConditionExpression = this._moddle.create('bpmn:FormalExpression', {});
-    this._businessObjInPanel.conditionExpression = conditionExpression;
+  private createConditionExpression(): void {
+    const conditionExpression: IConditionExpression = this.moddle.create('bpmn:FormalExpression', {});
+    this.businessObjInPanel.conditionExpression = conditionExpression;
   }
 
-  private _elementIsFlow(element: IFlowElement): boolean {
+  private elementIsFlow(element: IFlowElement): boolean {
     return element !== undefined && element !== null && element.$type === 'bpmn:SequenceFlow';
   }
 
-  private _init(): void {
+  private init(): void {
     if (
-      this._businessObjInPanel.conditionExpression &&
-      this._businessObjInPanel.conditionExpression.body !== undefined
+      this.businessObjInPanel.conditionExpression &&
+      this.businessObjInPanel.conditionExpression.body !== undefined
     ) {
-      this.condition = this._businessObjInPanel.conditionExpression.body;
+      this.condition = this.businessObjInPanel.conditionExpression.body;
     } else {
       this.condition = '';
     }
   }
 
-  private _publishDiagramChange(): void {
-    this._eventAggregator.publish(environment.events.diagramChange);
+  private publishDiagramChange(): void {
+    this.eventAggregator.publish(environment.events.diagramChange);
   }
 }
