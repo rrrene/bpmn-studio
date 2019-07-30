9c7cd942b0dad57debd077ee70f0a98e8cc47e60
Steffen <Steffen.knaup@5minds.de>
2019-07-30 10:18:11 +0200

    :rotating_light: Disable Eslint in Open ID Files

--- src/services/authentication-service/open-id/open-id-signin-response.ts
+++ src/services/authentication-service/open-id/open-id-signin-response.ts
@@ -1,9 +1,7 @@
+/* eslint-disable */
+
 // Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
-
-// tslint:disable:variable-name
-// tslint:disable:no-magic-numbers
-
 import {UrlUtility} from './open-id-url-utility';
 
 const OidcScope: string = 'openid';
