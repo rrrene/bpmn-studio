0691f12b8931657da871bcf86b059d189968e85a
Alex😎 <alexander.kasten@5minds.de>
2019-07-30 11:55:37 +0200

    :rotating_light: Fix errors for aurelia os

--- aurelia_project/native/os.js
+++ aurelia_project/native/os.js
@@ -1,9 +1,8 @@
+/* eslint-disable no-undef */
 if (window.nodeRequire) {
   define('os', () => {
     return window.nodeRequire('os');
   });
 } else {
-  define('os', () => {
-
-  });
+  define('os', () => {});
 }
