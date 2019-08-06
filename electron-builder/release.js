2cd54041987ee48ee797803c93f659e934c51173
Alex😎 <alexander.kasten@5minds.de>
2019-08-06 14:48:06 +0200

    :recycle: Return releaseChannel instead of alpha string

--- electron-builder/release.js
+++ electron-builder/release.js
@@ -8,7 +8,7 @@ function getReleaseChannel() {
   if (packageVersion.match(/-beta\./)) {
     releaseChannel = 'beta';
   }
-  return 'alpha';
+  return releaseChannel;
 }
 
 function getReleaseChannelSuffix() {
